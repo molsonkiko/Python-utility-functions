02246d18dc6fd7bf8e1c3fd508a6fbf8af79bfd5
molsonkiko <46202915+molsonkiko@users.noreply.github.com>
2021-03-16 21:14:43 -0700

    Update numpy_to_latex.py
    Now it handles float('nan') and np.inf type args

--- numpy_conversion/numpy_to_latex.py
+++ numpy_conversion/numpy_to_latex.py
@@ -6,6 +6,6 @@ def numpy_to_latex(mat,bracket_type='b'):
 	begin = r"$\begin{"+bracket_type+"matrix}"+'\n'
 	end_ = "\n"+r"\end{"+bracket_type + "matrix}$"
 	out_rows = not_whitespace_rows(strmat)
-	out_rows = [' & '.join(re.findall(numregex,row)) for row in out_rows]
+	out_rows = [' & '.join(re.findall(numregex,row,re.I)) for row in out_rows]
 	print(begin + ' \\\\ \n'.join(out_rows) + end_)
 	
