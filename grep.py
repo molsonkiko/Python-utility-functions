cd86261ddc6cffeca001a4c6d4a43cd211eef76e
molsonkiko <46202915+molsonkiko@users.noreply.github.com>
2021-05-26 20:03:24 -0700

    Update grep.py
    Added '-t' argument for getting number of files (and total size when used with '-s option).
    Also made it so that you can choose to return the results from process_grep_query as a Python object rather than printing them. This can be useful if for example you want to compute the sizes and number of files inside all the subdirectories of a directory (which would be inconvenient to do by printing alone).

--- grep.py
+++ grep.py
@@ -9,8 +9,8 @@ from commanum import format_bytes
 def fname_checker(fname,ext):
 	ext_begin = -len(fname)
 	for ind in range(1,len(fname)+1):
-		if fname[-ind]=='.':
 			ext_begin = ind
+		if fname[-ind]=='.':
 			break
 	return fname[:-ext_begin] + '.' + ext
 
@@ -61,11 +61,12 @@ grep.options = ('-a(ll file types)',
  '-o (match only entire words)',
  '-p (oPen all files in the final resultset with their default apps)',
  '-s(izes of files found)',
+ '-t (number of files found (and total size if -s arg used)',
  '-r(ecursive search)',
  '-v (display only things that DON\'T match)',
  '-w (write results of query to a JSON file)')
  
-def process_grep_query(query,has_warned_fname = False):
+def process_grep_query(query, mode = 'print', has_warned_fname = False):
 	try: #make the query
 		parsed_query = [re.findall(queryParser, x)[0] for x in query.split('-}}')]
 		options, regexes,fnames = list(zip(*parsed_query))
@@ -101,17 +102,26 @@ def process_grep_query(query,has_warned_fname = False):
 		resultset = modtime_d
 		del modtime_d
 	
-	if any(('s' in x) for x in options): #find file sizes
+	t_option = any(('t' in x) for x in options) # used -t option, get count of files and sum of sizes
+	if any(('s' in x) for x in options): # find file sizes
 		if not m_option:
 			sizes = [(f, filesize(f)) for f in resultset]
-			sizes.sort(key = lambda x: x[1], reverse = True)
-			sizes = [(x, format_bytes(y)) for x,y in sizes]
+			if t_option:
+				sizes = [len(resultset), format_bytes(sum(x[1] for x in sizes))]
+			else:
+				sizes.sort(key = lambda x: x[1], reverse = True)				
+				sizes = [(x, format_bytes(y)) for x,y in sizes]
 		else:
 			sizes = [f + (filesize(f[0]),) for f in resultset]
-			sizes.sort(key = lambda x: x[2], reverse = True)
-			sizes = [(x, y, format_bytes(z)) for x,y,z in sizes]
+			if t_option: 
+				sizes = [len(resultset), format_bytes(sum(x[2] for x in sizes))]
+			else:
+				sizes.sort(key = lambda x: x[2], reverse = True)
+				sizes = [(x, y, format_bytes(z)) for x,y,z in sizes]
 		resultset = sizes
 		del sizes
+	elif t_option:
+		resultset = len(resultset)
 		
 	for optset in options:
 		num = re.findall('\d+', optset)
@@ -121,35 +131,41 @@ def process_grep_query(query,has_warned_fname = False):
 	
 	if any(('p' in x) for x in options): #used -p option, open files
 		p_option = True
-		print(resultset)
+		if mode == 'print':
+			print(resultset)
 		opening_decision = 'y'
 		if len(resultset) > 5:
 			opening_decision = input(f"This resultset includes {len(resultset)} files. Are you sure you want to open all of them (y/n)?\n")
 		if opening_decision =='y':
-			##open each file with default application
+			## open each file with default application
 			for filename in resultset:
-				if isinstance(filename,tuple): #'-m' option used
+				if isinstance(filename,tuple): # '-m' option or '-s' option used
 					filename = filename[0]
 				os.startfile(filename) #only works on Windows unfortunately
 				#import subprocess
 				#subprocess.run([filename],shell=True,check=True) #alternative that probably generalizes better
 				#could also probably use subprocess.Popen(executable,filename) to specify an application
+	
 	if w_option: #used -w option, write results to files
 		import json
 		write_to_name = increment_name(fname_checker(write_to_name,'json'))
 		with open(write_to_name,'w') as f:
 			json.dump(resultset,f)
 		del write_to_name
-	if not (p_option | w_option): #just print results
-		print(resultset)
-	return has_warned_fname
+	
+	if mode == 'print':
+		if not (p_option | w_option): # just print results
+			print(resultset)
+		return has_warned_fname
+	return resultset
+
 
 def main():
 	has_warned_fname = False
 	args = sys.argv
 	if len(args) > 1: #first arg is always path to the module's file
 		query = ' '.join(args[1:])
-		has_warned_fname = process_grep_query(query,has_warned_fname)
+		has_warned_fname = process_grep_query(query, 'print', has_warned_fname)
 		return
 	print(helpstring)
 	while True:
@@ -180,17 +196,20 @@ def main():
 			print(os.listdir('.'))
 			continue
 		else: #parse the query as a grep query
-			has_warned_fname = process_grep_query(query,has_warned_fname)
+			has_warned_fname = process_grep_query(query, 'print', has_warned_fname)
 		print()
 		del query
 	return None
+
 	
 if __name__ == '__main__':
 	main()
 
+
 example_greps = ["-w 'foo.bar.csv'",
 					"-p -i 'jubar'",
 					"-p '.'"]
+
 
 fullGreps = ["-i -f -r 'zebra' /delicious -}} -30 'meat' -}} -w 'foo.bar.csv'",
 				"-i -f -r 'zebra' /delicious -}} -p -l 'meat'",
